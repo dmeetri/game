4b397713db9d4b10368a4cc1ac09d7bad9e8facb
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-12-27 07:00:18 +0900

    fix progress

--- scripts/core.js
+++ scripts/core.js
@@ -3,38 +3,58 @@ class Vector2D {
     this.x = x;
     this.y = y;
   }
+
+  normalize() {}
+
+  add() {}
 }
 
 class GameObject {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
+  constructor(x = 0, y = 0, width = 0, height = 0, sprite = "") {
+    this.position = Vector2D(x, y);
+    this.width = width;
+    this.height = height;
+    this.sprite = sprite;
+  }
+
+  setPos(newX, newY) {
+    this.x = newX;
+    this.y = newY;
   }
 
   get getPos() {
+    return this.position;
+  }
+
+  resize(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+
+  get getSize() {
     return {
-      x: this.x,
-      y: this.y,
+      width: this.width,
+      height: this.height,
     };
   }
 
-  render(deltaTime) {
-    throw new Error("Наследники должны определить метод render");
+  setSprite(newSprite) {
+    this.sprite = newSprite;
   }
 }
 
 class CollisionObject extends GameObject {
-  constructor(x, y) {
-    super(x, y);
+  constructor(x, y, width, height, sprite) {
+    super(x, y, width, height, sprite);
 
-    this.radius = 0;
+    this.colliderRadius = 0;
     this.isColliding = true;
   }
 }
 
 class Entity extends CollisionObject {
-  constructor(x, y, isColliding, radius) {
-    super(x, y, isColliding, radius);
+  constructor(x, y, isColliding, colliderRadius) {
+    super(x, y, isColliding, colliderRadius);
 
     this.speed = 0;
     this.direction = 0;
