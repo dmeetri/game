201b346fef062e9f667f993c98b90b9bd3827ba7
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-12-26 23:19:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# game
+My HTML CSS JS game
---
+++ js/core.js
@@ -0,0 +1,24 @@
+class Object {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+
+  update(deltatime) {}
+
+  render() {
+    throw new Error("Дочерние методы должны реализовать метод render");
+  }
+}
+
+class CollisionObject extends Object {
+  constructor(x = 0, y = 0, radius = 10) {
+    super(x, y);
+    this.radius = radius;
+  }
+}
